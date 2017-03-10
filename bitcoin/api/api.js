f490229001abb5a33ef82d0663b74a7d1ed83ba4
maximemartinez <maxime.martinez91@gmail.com>
2017-03-10 15:31:38 +0100

    ajout

--- bitcoin/api/api.js
+++ bitcoin/api/api.js
@@ -1,26 +1,11 @@
-/**
- * @author	Albin CAUDERLIER
- * @date	24/02/2017
- * 
- * Script jQuery appelant l'API de BlockCypher.com et affichant les données.
- * 
- */
-
-$(document).ready(function() {
-	$.ajax({
-		url : "https://api.blockcypher.com/v1/btc/main",
-		dataType : "json",
-		contentType : "application/json; charset=utf-8",
-		type : "GET",
-		timeout:	"5000",
-		async : false,
-
-		success : function(data) {
-			$('#bitcoin_block_number').append(data.height);
-		},
-
-		error : function(xhr, status, err) {
-			$('#bitcoin_block_number').append(err+" N/A");
-		}
-	});
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
 });
